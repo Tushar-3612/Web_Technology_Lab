5220c0703940408c0ab2efe2d3a64acf2956f95f
Nagare Tushar <145959296+Tushar-3612@users.noreply.github.com>
2025-09-08 22:25:44 +0530

    getdata.php

--- Assignment_4/LAB_WORK/getdata.php
+++ Assignment_4/LAB_WORK/getdata.php
@@ -1,28 +1,19 @@
- 
-<?php
+ <?php
 $name=$_POST["sname"];
 $marks=$_POST["smarks"];
 
 $conn=new mysqli("localhost","root","","info");
 if($conn){
-    $stat=$conn->prepare("insert into Userinfo (name,marks) values(?,?)");
+    $stat=$conn->prepare("insert into userinfo (Name, Mark) values(?, ?)");
     $stat->bind_param("si",$name,$marks);
     if($stat->execute()){
-        echo"Data is inserte succesfully";
-        
+        echo "Data is inserted successfully";
     }
-
 }else{
-    echo"Error in connection";
+    echo "Error in connection";
 }
 
-// i want to send response in json from php 
-//1.prepare data 
-//2. convert json 
-//3. configure header 
-
-$arr=["StudentName"=>$name,"StudentMArks"=>$marks];
+$arr=["StudentName"=>$name,"StudentMarks"=>$marks];
 $response=json_encode($arr);
-
+echo $response;
 ?>
- 
